527ff2ddf9f71d81193e823bc2aa246c336639fa
Igor Stalmach <igorstalmach@outlook.com>
2023-11-13 23:21:50 +0100

    Update achievements (#10)
    * Implement progress visibility
    
    * Refactor into separate component

---
+++ BudgetTracker/components/progress-card/IProgressCardProps.ts
@@ -0,0 +1,8 @@
+export interface IProgressCardProps {
+  title: string;
+  subtitle: string;
+  color: string;
+  icon: string;
+  currentlySpent: number;
+  totalBudget: number;
+}
--- BudgetTracker/screens/achievements/AchievementsScreen.tsx
+++ BudgetTracker/screens/achievements/AchievementsScreen.tsx
@@ -1,98 +1,54 @@
-import { ScrollView, View } from "react-native";
-import { Card, Icon, MD3Colors, ProgressBar, Text } from "react-native-paper";
+import { ScrollView } from "react-native";
 import { AchievementsScreenStyle } from "./AchievementsScreen.style";
+import { ProgressCard } from "../../components/progress-card/ProgressCard";
 
 export const AchievementsScreen = () => {
   const mockedAchievements = [
     {
       title: "Transportation",
       subtitle: "$20 per day",
-      progress: 0.4,
       color: "#F250FE",
       icon: "car-hatchback",
+      currentlySpent: 10,
+      totalBudget: 200,
     },
     {
       title: "Education",
       subtitle: "$10 per day",
-      progress: 1.2,
       color: "#EC4B0B",
       icon: "book-open-page-variant",
+      currentlySpent: 35,
+      totalBudget: 100,
     },
     {
       title: "Food",
       subtitle: "$40 per day",
-      progress: 0.8,
       color: "#3D3597",
       icon: "food",
+      currentlySpent: 50,
+      totalBudget: 40,
     },
     {
       title: "Rent",
       subtitle: "$20 per day",
-      progress: 0.4,
       color: "#007107",
       icon: "currency-usd",
+      currentlySpent: 20,
+      totalBudget: 25,
     },
   ];
 
   return (
     <ScrollView style={AchievementsScreenStyle.container}>
       {mockedAchievements.map((achievement) => (
-        <Card style={AchievementsScreenStyle.card}>
-          <Card.Content>
-            <View style={AchievementsScreenStyle.titleContainer}>
-              <View style={AchievementsScreenStyle.iconContainer}>
-                <View style={AchievementsScreenStyle.iconWrapper}>
-                  <Icon
-                    source={achievement.icon}
-                    color={achievement.color}
-                    size={20}
-                  />
-                </View>
-              </View>
-              <View>
-                <Text
-                  variant={"titleLarge"}
-                  style={AchievementsScreenStyle.titleFont}
-                >
-                  {achievement.title}
-                </Text>
-                <Text variant={"labelLarge"}>{achievement.subtitle}</Text>
-              </View>
-            </View>
-          </Card.Content>
-          <Card.Content>
-            <View style={AchievementsScreenStyle.progressBarContainer}>
-              <ProgressBar
-                progress={achievement.progress}
-                color={achievement.color}
-                style={AchievementsScreenStyle.progressBar}
-              />
-            </View>
-          </Card.Content>
-          <Card.Content>
-            {achievement.progress > 0 && achievement.progress < 1 ? (
-              <View style={AchievementsScreenStyle.statusContainer}>
-                <Icon
-                  source="check-circle"
-                  color={MD3Colors.primary0}
-                  size={20}
-                />
-                <Text variant={"labelLarge"}>You're on track!</Text>
-              </View>
-            ) : (
-              <View style={AchievementsScreenStyle.statusContainer}>
-                <Icon
-                  source="close-circle"
-                  color={MD3Colors.error50}
-                  size={20}
-                />
-                <Text variant={"labelLarge"}>
-                  Uh oh! You exceeded your budget
-                </Text>
-              </View>
-            )}
-          </Card.Content>
-        </Card>
+        <ProgressCard
+          title={achievement.title}
+          subtitle={achievement.subtitle}
+          color={achievement.color}
+          icon={achievement.icon}
+          currentlySpent={achievement.currentlySpent}
+          totalBudget={achievement.totalBudget}
+        />
       ))}
     </ScrollView>
   );
