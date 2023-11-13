21acfa5171973b2fbae15f715fdcb3129887b231
Igor Stalmach <igorstalmach@outlook.com>
2023-11-13 23:19:23 +0100

    Refactor into separate component

--- BudgetTracker/screens/achievements/AchievementsScreen.tsx
+++ BudgetTracker/screens/achievements/AchievementsScreen.tsx
@@ -1,6 +1,6 @@
-import { ScrollView, View } from "react-native";
-import { Card, Icon, MD3Colors, ProgressBar, Text } from "react-native-paper";
+import { ScrollView } from "react-native";
 import { AchievementsScreenStyle } from "./AchievementsScreen.style";
+import { ProgressCard } from "../../components/progress-card/ProgressCard";
 
 export const AchievementsScreen = () => {
   const mockedAchievements = [
@@ -9,110 +9,46 @@ export const AchievementsScreen = () => {
       subtitle: "$20 per day",
       color: "#F250FE",
       icon: "car-hatchback",
-      currentSpending: 10,
-      maxSpending: 200,
+      currentlySpent: 10,
+      totalBudget: 200,
     },
     {
       title: "Education",
       subtitle: "$10 per day",
       color: "#EC4B0B",
       icon: "book-open-page-variant",
-      currentSpending: 35,
-      maxSpending: 100,
+      currentlySpent: 35,
+      totalBudget: 100,
     },
     {
       title: "Food",
       subtitle: "$40 per day",
       color: "#3D3597",
       icon: "food",
-      currentSpending: 50,
-      maxSpending: 40,
+      currentlySpent: 50,
+      totalBudget: 40,
     },
     {
       title: "Rent",
       subtitle: "$20 per day",
       color: "#007107",
       icon: "currency-usd",
-      currentSpending: 20,
-      maxSpending: 25,
+      currentlySpent: 20,
+      totalBudget: 25,
     },
   ];
 
   return (
     <ScrollView style={AchievementsScreenStyle.container}>
       {mockedAchievements.map((achievement) => (
-        <Card style={AchievementsScreenStyle.card}>
-          <Card.Content>
-            <View style={AchievementsScreenStyle.titleContainer}>
-              <View style={AchievementsScreenStyle.iconContainer}>
-                <View style={AchievementsScreenStyle.iconWrapper}>
-                  <Icon
-                    source={achievement.icon}
-                    color={achievement.color}
-                    size={20}
-                  />
-                </View>
-              </View>
-              <View>
-                <Text
-                  variant={"titleLarge"}
-                  style={AchievementsScreenStyle.titleFont}
-                >
-                  {achievement.title}
-                </Text>
-                <Text variant={"labelLarge"}>{achievement.subtitle}</Text>
-              </View>
-            </View>
-          </Card.Content>
-          <Card.Content>
-            <View style={AchievementsScreenStyle.progressBarContainer}>
-              <Text
-                style={AchievementsScreenStyle.progressBarText}
-                variant={"labelLarge"}
-              >
-                {achievement.currentSpending + "PLN"}
-              </Text>
-              <View style={AchievementsScreenStyle.progressBarWrapper}>
-                <ProgressBar
-                  progress={
-                    achievement.currentSpending / achievement.maxSpending
-                  }
-                  color={achievement.color}
-                  style={AchievementsScreenStyle.progressBar}
-                />
-              </View>
-              <Text
-                style={AchievementsScreenStyle.progressBarText}
-                variant={"labelLarge"}
-              >
-                {achievement.maxSpending + "PLN"}
-              </Text>
-            </View>
-          </Card.Content>
-          <Card.Content>
-            {achievement.currentSpending / achievement.maxSpending < 1 ? (
-              <View style={AchievementsScreenStyle.statusContainer}>
-                <Icon
-                  source="check-circle"
-                  color={MD3Colors.primary0}
-                  size={20}
-                />
-                <Text variant={"labelLarge"}>You're on track!</Text>
-              </View>
-            ) : (
-              <View style={AchievementsScreenStyle.statusContainer}>
-                <Icon
-                  source="close-circle"
-                  color={MD3Colors.error50}
-                  size={20}
-                />
-                <Text variant={"labelLarge"}>
-                  Uh oh! You exceeded your budget
-                </Text>
-              </View>
-            )}
-          </Card.Content>
-        </Card>
+        <ProgressCard
+          title={achievement.title}
+          subtitle={achievement.subtitle}
+          color={achievement.color}
+          icon={achievement.icon}
+          currentlySpent={achievement.currentlySpent}
+          totalBudget={achievement.totalBudget}
+        />
       ))}
     </ScrollView>
   );
