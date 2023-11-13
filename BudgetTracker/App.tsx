daec1cbf53269f8293247ca858f778ade0fe121d
Damian0401 <szkola_abc@o2.pl>
2023-11-13 20:00:12 +0100

    Added user screen

--- BudgetTracker/App.tsx
+++ BudgetTracker/App.tsx
@@ -4,6 +4,9 @@ import HomeScreen from "./screens/home/HomeScreen";
 import CalendarScreen from "./screens/calendar/CalendarScreen";
 import * as React from "react";
 import { PaperProvider } from "react-native-paper";
+import Constants from "./common/utils/constants";
+import UserScreen from "./screens/user/UserScreen";
+import { SafeAreaView } from "react-native";
 
 const Stack = createNativeStackNavigator();
 
@@ -11,9 +14,13 @@ export default function App() {
   return (
     <PaperProvider>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Calendar" component={CalendarScreen} />
+        <Stack.Navigator initialRouteName={Constants.Screens.User}>
+          <Stack.Screen name={Constants.Screens.Home} component={HomeScreen} />
+          <Stack.Screen
+            name={Constants.Screens.Calender}
+            component={CalendarScreen}
+          />
+          <Stack.Screen name={Constants.Screens.User} component={UserScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </PaperProvider>
