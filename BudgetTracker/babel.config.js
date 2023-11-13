7ed915c92a6ec7b069488a62c2008daa7a9fcb08
Igor Stalmach <igorstalmach@outlook.com>
2023-11-13 18:41:08 +0100

    Add React Native Paper

--- BudgetTracker/babel.config.js
+++ BudgetTracker/babel.config.js
@@ -2,5 +2,12 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
+    env: {
+      production: {
+        // The plugin automatically rewrites the import statements so that
+        // only the modules you use are imported instead of the whole library.
+        plugins: ["react-native-paper/babel"],
+      },
+    },
   };
 };
