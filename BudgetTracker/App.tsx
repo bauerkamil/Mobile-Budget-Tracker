9754ef0bd2699df6e4de7923957d4b1c44a9b98a
Igor Stalmach <igorstalmach@outlook.com>
2023-11-13 18:50:01 +0100

    Add custom components

--- BudgetTracker/App.tsx
+++ BudgetTracker/App.tsx
@@ -1,17 +1,21 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './screens/home/HomeScreen';
-import CalendarScreen from './screens/calendar/CalendarScreen';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/home/HomeScreen";
+import CalendarScreen from "./screens/calendar/CalendarScreen";
+import * as React from "react";
+import { PaperProvider } from "react-native-paper";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Calendar" component={CalendarScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Calendar" component={CalendarScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
--- BudgetTracker/screens/home/HomeScreen.tsx
+++ BudgetTracker/screens/home/HomeScreen.tsx
@@ -1,18 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, Text, View } from 'react-native';
-import { IHomeScreenProps } from './IHomeScreenProps';
-import { HomeScreenStyle } from './HomeScreen.style';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import { IHomeScreenProps } from "./IHomeScreenProps";
+import { HomeScreenStyle } from "./HomeScreen.style";
+import { ActivityIndicator, MD2Colors, Button } from "react-native-paper";
 
 export default function HomeScreen(props: IHomeScreenProps) {
-  const {navigation} = props;
+  const { navigation } = props;
   return (
-      <View style={HomeScreenStyle.container}>
-        <Text>Open up App.tsx to start working on your app! this is home screen</Text>
-        <Button
-          title="Go to Calendar"
-          onPress={() => navigation.navigate('Calendar')}
-        />
-        <StatusBar style="auto" />
-      </View>
+    <View style={HomeScreenStyle.container}>
+      <Text>
+        Open up App.tsx to start working on your app! this is home screen
+      </Text>
+      <StatusBar style="auto" />
+      <ActivityIndicator animating={true} color={MD2Colors.red800} />
+      <Button
+        icon="camera"
+        mode="contained"
+        onPress={() => console.log("Pressed")}
+      >
+        Press me
+      </Button>
+    </View>
   );
 }
