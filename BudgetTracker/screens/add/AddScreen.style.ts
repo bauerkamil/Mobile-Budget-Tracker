26a37bb6f1390fa2747bebf9e198e4337ab02ed6
Damian Gnieciak <77583760+Damian0401@users.noreply.github.com>
2023-11-13 22:43:04 +0100

    Added AddScreen (#8)

---
+++ BudgetTracker/screens/add/AddScreen.style.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const AddScreenStyle = StyleSheet.create({
+  container: {
+    width: "100%",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    flexDirection: "column",
+  },
+  tab: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
